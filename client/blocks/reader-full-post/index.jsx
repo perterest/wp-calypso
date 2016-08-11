20afc850c9230cac2d8098cfff012fc07df63c20
nssw <nssw@users.noreply.github.com>
2016-08-11 11:46:06 +0200

    Fix no-unused-vars
    ref commit: 524aa9a2f6b32a80ab9e396fe044dd32a9616908

--- client/blocks/reader-full-post/index.jsx
+++ client/blocks/reader-full-post/index.jsx
@@ -25,7 +25,7 @@ import AuthorCompactProfile from 'blocks/author-compact-profile';
 
 export class FullPostView extends React.Component {
 	render() {
-		const { post, site, feed } = this.props;
+		const { post, site, feed } = this.props; // eslint-disable-line no-unused-vars
 		/*eslint-disable react/no-danger*/
 		return (
 			<Main className="reader-full-post">
