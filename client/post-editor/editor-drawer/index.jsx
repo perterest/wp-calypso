b20c9d363babf9ad490383cc481e731f94009a38
Copons <copons@users.noreply.github.com>
2016-08-12 13:00:05 +0200

    Show Copy Post drawer only for posts

--- client/post-editor/editor-drawer/index.jsx
+++ client/post-editor/editor-drawer/index.jsx
@@ -263,6 +263,11 @@ const EditorDrawer = React.createClass( {
 	},
 
 	renderCopyPost: function() {
+		// TODO: replace with currentPostTypeSupports( 'copy' );
+		if ( 'post' !== this.props.type ) {
+			return;
+		}
+
 		return (
 			<EditorDrawerCopyPost />
 		);
